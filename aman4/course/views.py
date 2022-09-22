f60de13175844a35c7ed17f02b287900729e2f9d
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-09-23 00:35:49 +0530

    templates edited with css and added in views

--- aman4/course/views.py
+++ aman4/course/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def learn_Django(request):
+    return render(request,'courseone.html')
+
+def learn_Python(request):
+    return render(request,'coursetwo.html')
--- aman4/fees/views.py
+++ aman4/fees/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def fees_Django(request):
+    return render(request,'feesone.html')
+
+def fees_Python(request):
+    return render(request,'feestwo.html')
