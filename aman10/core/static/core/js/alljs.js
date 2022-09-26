99a4fd1c9271e7f72b0b39c10e72ce219c577b8d
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-09-27 01:25:41 +0530

    doing my own thing that click me will show the url

---
+++ aman10/core/static/core/js/alljs.js
@@ -0,0 +1,10 @@
+function dispabout(){
+    // var url = 'http://127.0.0.1:8000/about/';
+    document.location.href='about/'; //Location gets on root folder's url and then matches if any url is there of same name then show that url, add the url after the given url 
+    // element.addEventListener('clickme',function(){
+    // location.href = url;
+// });
+}
+function dispdjango(){
+    document.location.href='django/django';
+}
