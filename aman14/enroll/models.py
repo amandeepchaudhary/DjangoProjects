1bf3876aeb7384d5ef03522a2355a14b9e8e7671
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-11 22:52:14 +0530

    models

--- aman14/enroll/models.py
+++ aman14/enroll/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class students(models.Model):
+    stuid = models.IntegerField(primary_key=True)
+    stuname = models.CharField(max_length=90)
+    stuclass = models.IntegerField()
+    stumail = models.EmailField(default='Email is not given')
+    stuphone = models.BigIntegerField()
