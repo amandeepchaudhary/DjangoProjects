24c380572b2b5c656aaedf5ff62e3332588f9920
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-11 22:52:03 +0530

    views in python for getting the models class

--- aman14/enroll/views.py
+++ aman14/enroll/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from enroll.models import students
 # Create your views here.
 def stuhome(request):
     return render(request,'enroll/home.html')
+
 def stuinfo(request):
-    return render(request,'enroll/studetails.html')
+    stud = students.objects.all()   #all() = returns Query set
+    print('myoutput',stud)
+    return render(request,'enroll/studetails.html',{'stu':stud})
