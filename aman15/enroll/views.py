0a4039783a29f0cae236447bf5ef232fbb054f70
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-15 16:32:57 +0530

    why the aman15 made

--- aman15/enroll/views.py
+++ aman15/enroll/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Data base in admin that is automatically installed in django so that is why we dont need to do anything except the commands "python manage.py makemigrations" "python manage.py showmigrations" "python manage.py migrate" and go to the ip address and add "/admin/" the database will open automatically 
