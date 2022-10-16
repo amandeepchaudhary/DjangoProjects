94ff1c7d7e447e54959a59a14155d02e94195b2d
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-16 23:11:33 +0530

    Loop Form visible Fields with the for loop

--- aman22/enroll/forms.py
+++ aman22/enroll/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class StudentRegistration(forms.Form):
-    first_name = forms.CharField(initial="aman@aman.com", help_text="this is the help field - hum char field m likh rahe h uske constructor mai Aur sara code 'View Page Source' m bhi nhi dikh rha hoga")  # the "_" is converted into " "
+    first_name = forms.CharField()  # the "_" is converted into " "
     email = forms.EmailField()  # Not showing in web page because humne sara code hi first_name ka kara h so we study loop
-
+    phone = forms.IntegerField()
     # iss class ka object banega views.py mai
