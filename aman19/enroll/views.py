c1fbf37717ba4a355c9e955a51f0e09b09d40245
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-16 21:29:13 +0530

    changes in label tag with help constructor in view

--- aman19/enroll/views.py
+++ aman19/enroll/views.py
@@ -5,5 +5,8 @@ from enroll.forms import StudentRegistration
 
 
 def StuReg(request):
-    sr = StudentRegistration()
+    sr = StudentRegistration(auto_id=True, label_suffix=' ', initial={'name': 'aman', 'email': 'aman@aman.com'}) 
+    # auto_id changes the id in html code we have three options True/False/ any string , In True the auto id will be the same as the name of the label , In False the auto id and label tag will be removed , And in any other String that is "aman" the id will change like aman_name  
+    # label_suffix is the ":" symbol which is next to the english word that is present in the label
+    # initial is used to give a initial value to the label in runtime, it is given in the form of dictionary where the key is the name of the label
     return render(request, 'enroll/userregistration.html', {'form': sr})
