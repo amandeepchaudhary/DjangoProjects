fbf6badc147b0454475e69ca256938d45f9338f8
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-16 21:29:03 +0530

    changes in form- labelsufx,id,initial,_tospace

--- aman19/enroll/forms.py
+++ aman19/enroll/forms.py
@@ -4,5 +4,6 @@ class StudentRegistration(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     phone = forms.IntegerField()
+    first_name = forms.CharField()  # the "_" is converted into " "
 
     # iss class ka object banega views.py mai
