bfa6740f656672b47163ecb2b22d363bca4288b4
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-16 22:42:54 +0530

    using the attributes of form and char in fields

--- aman21/enroll/views.py
+++ aman21/enroll/views.py
@@ -5,7 +5,7 @@ from enroll.forms import StudentRegistration
 
 
 def StuReg(request):
-    sr = StudentRegistration(auto_id=True, label_suffix=' ', initial={'name': 'aman', 'email': 'aman@aman.com'}) 
+    sr = StudentRegistration() 
     sr.order_fields(field_order=[ 'email', 'name', 'first_name', 'phone'])  # sr.order_fields(field_order=None) is set by default but we can reorder it 
     # auto_id changes the id in html code we have three options True/False/ any string , In True the auto id will be the same as the name of the label , In False the auto id and label tag will be removed , And in any other String that is "aman" the id will change like aman_name  
     # label_suffix is the ":" symbol which is next to the english word that is present in the label
