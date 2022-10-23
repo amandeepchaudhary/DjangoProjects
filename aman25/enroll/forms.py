e5b5b082ebd506effaac12ac0b15fcb55223dd7c
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-24 00:46:38 +0530

    Widget and attr in it + get/post

--- aman25/enroll/forms.py
+++ aman25/enroll/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
 class student(forms.Form):
-    name = forms.CharField(widget=forms.EmailInput())
+    name = forms.CharField(widget=forms.TextInput(attrs={'class':'csssheet', 'id':'uniqueid'}))
+    # we use the "widget = forms." for the things which is not available in "form." || .Textarea, .passwordinput, .hiddeninput, .checkboxinput, .fileinput, .textinput 
+    # attr in widgets used for the changes in attributes of the widgets like adding a css and changing the id name in the field, Jo bhi attributes hum change krna chahte h unhe pakdo view page source aur attr me key ki tarh likh k krdo change.
