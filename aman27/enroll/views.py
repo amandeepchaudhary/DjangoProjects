015f7508aff729aac818c3a2854cbc160c43eb44
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-25 16:20:17 +0530

    is_valid then cleaned_data then single single data

--- aman27/enroll/views.py
+++ aman27/enroll/views.py
@@ -5,5 +5,22 @@ from enroll.forms import Student
 
 
 def stureg(request):
-    st = Student()  # This value will execute at runtime so it will overwrite the values given in forms.py
+    if request.method == 'POST':
+        st = Student(request.POST)  # This value will execute at runtime so it will overwrite the values given in forms.py
+        if st.is_valid():
+            print('Form Validated')
+            print(st)
+            print('ye post se ayya hai')
+            print(st.cleaned_data)
+            name = st.cleaned_data['name']
+            email = st.cleaned_data['email']
+            password = st.cleaned_data['password']
+            print('Name:', name)
+            print('Email:', email)
+            print('Password:', password)
+    
+    else:
+        st = Student()
+        print('ye get se ayya hai')
+
     return render(request, 'enroll/userregistration.html', {'form': st})
