5b57a7c3685c0e9df8977e9a48f9634f7953bbe2
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-25 19:15:24 +0530

    why we need another page after submit click

--- aman27/enroll/views.py
+++ aman27/enroll/views.py
@@ -18,6 +18,7 @@ def stureg(request):
             print('Name:', name)
             print('Email:', email)
             print('Password:', password)
+            st = student()  # If we give this then there is a chance that it will submit twice so for preventing that to happen we need to give a seperate page on which we go after giving the details.
     
     else:
         st = Student()
--- aman28/enroll/views.py
+++ aman28/enroll/views.py
@@ -14,6 +14,7 @@ def studetails(request):
                 print('Name:', name)
                 print('Email:', email)
                 print('Password:', password)
+                st = student()  # If we give this then there is a chance that it will submit twice so for preventing that to happen we need to give a seperate page on which we go after giving the details.
     else:
         st = student()
 
