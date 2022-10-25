e573e51ddd61aec6d772cb7690d500746b9c627a
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-26 04:57:44 +0530

    giving validators like signs restrictions and name starts with

--- aman33/enroll/forms.py
+++ aman33/enroll/forms.py
@@ -2,6 +2,16 @@ from django.core import validators
 from django import forms
 
 
+def starts_with_a(value):
+    if value[0]!='a':
+        raise forms.ValidationError("Name should start with 'a'")
+
+def sign_not_allowed(value):
+    signs = ['!','@','#','$','%','^','&','*','(',')','_','-','+','=']
+    for sign in signs: 
+        if sign in value:
+            raise forms.ValidationError('Enter without signs')
+
 class student(forms.Form):
-    name = forms.CharField(validators=[validators.MaxLengthValidator(10)])
+    name = forms.CharField(validators=[starts_with_a, sign_not_allowed])
     email = forms.EmailField()
