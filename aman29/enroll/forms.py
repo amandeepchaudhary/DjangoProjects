e3358c5c96a00069dd5c0d0ade5d3d36f58d0c7a
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-26 03:11:40 +0530

    form fields in forms.py and showing in terminal by views.py

--- aman29/enroll/forms.py
+++ aman29/enroll/forms.py
@@ -2,4 +2,21 @@ from django import forms
 
 
 class student(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(min_length=3, max_length=25, strip= True,empty_value='This is Given Empty Value', error_messages={'required':'Enter your name!'})
+    # Everything that is showing that the data is validated or not in the url is from the .is_valid in views.py  
+    # Strip is True in deafult mode and it does not allow the data to have white spaces if we give it as False then it will allow the data to have the white spaces 
+
+    roll = forms.IntegerField(max_value=20, min_value=1)
+    
+    price = forms.DecimalField(max_value=20, min_value=1, max_digits=4, decimal_places=2)
+
+    rate = forms.FloatField(max_value=20, min_value=1)
+    comment = forms.SlugField()
+    email = forms.EmailField(max_length=20, min_length=1)
+    website = forms.URLField(max_length=20, min_length=1)
+    password = forms.CharField(max_length=20, min_length=1, widget= forms.PasswordInput())
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows':2, 'cols':40}))
+    feedback = forms.CharField(max_length=50, min_length=5, widget=forms.Textarea(attrs={'class':'somecss1 somecss2', 'id':'uniqueid', 'rows':2, 'cols':40}))
+    notes = forms.CharField(widget=forms.Textarea(attrs={'rows':2, 'cols':40}))
+
+    agree = forms.BooleanField(label='I Agree', label_suffix=' ', error_messages={'required':'Accept the Terms and Conditions for next step'})
--- aman29/enroll/views.py
+++ aman29/enroll/views.py
@@ -10,6 +10,19 @@ def studetails(request):
         st = student(request.POST)
         if st.is_valid():
             print('Form Validated')
+            print('Name:', st.cleaned_data['name'])  # Key in forms
+            print('Roll:', st.cleaned_data['roll'])  # Key in forms
+            print('Price:', st.cleaned_data['price'])  # Key in forms
+            print('Rate:', st.cleaned_data['rate'])  # Key in forms
+            print('Comment:', st.cleaned_data['comment'])  # Key in forms
+            print('Email:', st.cleaned_data['email'])  # Key in forms
+            print('Website:', st.cleaned_data['website'])  # Key in forms
+            print('Password:', st.cleaned_data['password'])  # Key in forms
+            print('Description:', st.cleaned_data['description'])  # Key in forms
+            print('Feedback:', st.cleaned_data['feedback'])  # Key in forms
+            print('Notes:', st.cleaned_data['notes'])  # Key in forms
+
+            print('Agree:', st.cleaned_data['agree'])  # Key in forms
     else:
         st = student()
 
