6cde8d885f957d20e794af3f238921bea81446b3
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-26 04:02:38 +0530

    Form Fields Validation manually

--- aman30/enroll/forms.py
+++ aman30/enroll/forms.py
@@ -5,7 +5,7 @@ class student(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     password = forms.CharField(widget= forms.PasswordInput())
-    def clean_name(self):
+    def clean_name(self):  # Field Validation
         signs = ['@','!','#','%','^','&','*','(',')','_','-','=','+']
         valname = self.cleaned_data['name']
         if len(valname)<4:
