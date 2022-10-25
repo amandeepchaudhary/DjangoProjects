8a21201dbb2499bc7afdf2405dd4ada3aebdec11
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-26 03:58:57 +0530

    adding a validation when user enters a sign at the place of name

--- aman30/enroll/forms.py
+++ aman30/enroll/forms.py
@@ -6,7 +6,12 @@ class student(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget= forms.PasswordInput())
     def clean_name(self):
+        signs = ['@','!','#','%','^','&','*','(',')','_','-','=','+']
         valname = self.cleaned_data['name']
         if len(valname)<4:
             raise forms.ValidationError('Enter more than or equal 4 char')
+        for si in signs:  # we want to get a error message when user input the signs at the place of name 
+            print(si)
+            if si in valname:
+                raise forms.ValidationError('Enter right name')
         return valname
