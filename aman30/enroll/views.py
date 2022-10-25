1a7a9b8a964a2b54f4eb2ef2cf7f271b24bf31b9
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-26 03:33:13 +0530

    validating the name according to us in forms.py and views.py

---
+++ aman30/enroll/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from enroll.forms import student
+
+
+# Create your views here.
+
+
+def studetails(request):
+    if request.method == 'POST':
+        st = student(request.POST)
+        if st.is_valid():
+            print('Form Validated')
+            print('Name:', st.cleaned_data['name'])  # Key in forms
+            print('Email:', st.cleaned_data['email'])  # Key in forms
+            print('Password:', st.cleaned_data['password'])  # Key in forms
+    else:
+        st = student()
+
+    return render(request, 'enroll/studetails.html', {'form':st})
