9465b0210a9ef80847294d9e89fa919d8bda50d6
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-27 16:55:15 +0530

    in views make model class obj then give var of cleandata as data and save()

--- aman37/enroll/views.py
+++ aman37/enroll/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from enroll.forms import student
 from django.http import HttpResponseRedirect
+from enroll.models import user
+
 
 # Create your views here.
+
+
 def success(request):
     return render(request, 'enroll/success.html')
 
@@ -13,6 +17,11 @@ def sturegistration(request):
             nm = st.cleaned_data['name']
             em = st.cleaned_data['email']
             ps = st.cleaned_data['password']
+            reg = user(name=nm, email=em, password=ps)
+            # reg = user(id=1, name=nm, email=em, password=ps) now it will update the details of the id=1
+            reg.save()
+            reg = user(id=1)  # The user id=1 will be deleted bcoz of
+            reg.delete()    # <<<<<this operation
             return HttpResponseRedirect('/success/')
             
     else:        
