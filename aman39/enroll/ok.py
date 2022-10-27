eb352f8a81f6142221eeef89b0ad9a26252c6a7b
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-27 23:18:55 +0530

    added labels and error in forms and admin added also for database

---
+++ aman39/enroll/ok.py
@@ -0,0 +1,8 @@
+dicti = {'aman': 'male', 'ishu': 'female', 'sunita': 'female'}
+listi = []
+for k in dicti:
+    print(dicti[k])
+    listi.append(k)
+    print(k)
+    print(listi)
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
