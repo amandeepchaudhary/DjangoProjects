0db4f1d41ea9f26d2a63af47c0b04b361649d17c
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-27 23:44:25 +0530

    removed dictionary practice ok.py

--- aman40/enroll/ok.py
+++
@@ -1,8 +0,0 @@
-dicti = {'aman': 'male', 'ishu': 'female', 'sunita': 'female'}
-listi = []
-for k in dicti:
-    print(dicti[k])
-    listi.append(k)
-    print(k)
-    print(listi)
-    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
