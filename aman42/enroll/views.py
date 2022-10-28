d7f908c83728a153338d5cc821fb096bf4ab319a
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-10-28 23:24:18 +0530

    giving url with value in template and modify views and urls

--- aman42/enroll/views.py
+++ aman42/enroll/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'enroll/home.html')
+
+def details(request, my_id):
+    student = {'id': my_id}
+    return render(request, 'enroll/students.html', student)
