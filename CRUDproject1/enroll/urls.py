79580d4618c328c049f758c0502cc15636866d0e
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-11-07 01:41:03 +0530

    CRUD Project Done!!!

--- CRUDproject1/enroll/urls.py
+++ CRUDproject1/enroll/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from enroll import views
 
 urlpatterns = [
-    path('', views.add_show),
+    path('', views.add_show, name="addshow"),
+    path('update/<int:id>/', views.update_edit, name="updatedata"),
+    path('delete/<int:id>/', views.delete_stu, name="deletedata"),
 ]
--- CRUDproject1/enroll/views.py
+++ CRUDproject1/enroll/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from enroll.forms import StudentRegistration
+from enroll.models import User
 
 # Create your views here.
+
+# This will Add and Show
 def add_show(request):
-    return render(request, 'enroll/addandshow.html')
+    if request.method == 'POST':
+        fm = StudentRegistration(request.POST)  # Used the form import
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']  # Used the Model import
+            em = fm.cleaned_data['email']  # Used the Model import
+            pw = fm.cleaned_data['password']  # Used the Model import
+            #Object of Model Class==
+            reg = User(name=nm, email=em, password=pw)
+            reg.save()
+            fm = StudentRegistration()
+    else:
+        fm = StudentRegistration()
+    stud = User.objects.all()
+    return render(request, 'enroll/addandshow.html', {'form': fm, 'stu': stud})
+
+# This will Update/Edit
+def update_edit(request, id):  # id k sath dynamic url kyuki specific data pr kaam krna h isliye
+    if request.method == 'POST':
+        ud = User.objects.get(pk=id)
+        fm = StudentRegistration(request.POST, instance = ud)  #This is done for getting a form having all the details of the given id
+        if fm.is_valid():
+            fm.save()
+    else:
+        ud = User.objects.get(pk=id)
+        fm = StudentRegistration(instance = ud)
+    return render(request, 'enroll/update.html', {'form':fm})
+
+
+# This will Delete
+
+def delete_stu(request, id):
+    if request.method == 'POST':
+        dt = User.objects.get(pk=id)
+        dt.delete()
+        return HttpResponseRedirect('/')
