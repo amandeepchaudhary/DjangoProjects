bc4b03c87669b28a1bfa4f279694a438693bf887
Amandeep Singh <amanchaudhary.ac1@gmail.com>
2022-11-12 01:42:24 +0530

    Calculator in Python

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+a = float(input('Enter First Number:'))
+b = input('Select What you want to do with the Number +,-,*,/ :')
+c = float(input('Enter Second Number:'))
+
+if b=='+':
+    print('Your Answer is:', a+c)
+if b=='-':
+    print('Your Answer is:', a-c)
+if b=='*':
+    print('Your Answer is:', a*c)
+if b=='/':
+    print('Your Answer is:', a/c)
