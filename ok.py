a8129d9a9edd5cb7e0cde5fc21ff896756fb9e3b
amandeep <amanchaudhary.ac1@gmail.com>
2023-01-17 17:38:48 +0530

    2 list ele common-valid,Palindrome,typecheck-basic

--- ok.py
+++ ok.py
@@ -7,8 +7,61 @@ for k in dicti:
     print(listi)
     print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
-di = {'aman':'male','hitesh':'male'}
-li=[]
-for name,gen in di.items():
-    li.append((name,gen))
-print(li)
+# di = {'aman':'male','hitesh':'male'}
+# li=[]
+# aman =di.get
+# print(aman)
+# for name,gen in di.items():
+#     li.append((name,gen))
+# print(li)
+# print("@$$$$$$$$$$$$$$$$$$$$")
+# for gen in di.get():
+#     li.append(gen)
+# print(li)
+
+# ?Diksha's Lab Tasks
+# Question 4: Write a Program which takes two lists as inputs and validate if they have atleast one common member
+def fun():
+    l1=[]
+    l2=[]
+    num=int(input("Enter the Number of items you want to add in the L1:"))
+    for i in range(num):
+        l1.append(input(f"Enter the value to add in L1 at index {i}:"))
+
+    num2=int(input("Enter the Number of items you want to add in the L2:"))
+    for i1 in range(num2):
+        l2.append(input(f"Enter the value to add in L2 at index {i1}:"))
+        
+    print(l1)
+    print(l2)
+
+    execute =0
+    
+    for ele in l1:
+        if ele in l2:
+            execute = execute + 1
+    if not execute:
+        print("List is Invalid")
+    else:
+        print("List is Valid",l1+l2)
+    
+
+# fun()
+
+# Question 3: Write a program to check if the input number is a palindrome number or not
+def Palindrome():
+    number = int(input("Enter a Number to Check whether it is a Palindrome or Not:"))
+    reverse = 0
+    orinum = number
+    # for i in range(number):
+    while number>0:
+        reverse = (reverse*10)+(number%10)
+        number = number//10
+    
+    if orinum==reverse:
+        print("This number is a Palindrome")
+    else:
+        print("This number is not a Palindrome")
+
+
+Palindrome()
